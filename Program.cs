f66284f9ddb2b878897f902a9213b234393dd988
Kalpesh.Misry <kalpesh.mistry@ssp-worldwide.com>
2026-03-20 11:35:19 +0000

    DEV - Button text change

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ app.MapGet("/", () => Results.Content(@"<!DOCTYPE html>
 <body>
     <div class='container'>
         <h1>Lottery Numbers Generator</h1>
-        <button onclick='generateNumbers()'>Good Luck !</button>
+        <button onclick='generateNumbers()'>Fingers crossed !</button>
         <div class='numbers' id='numbers'></div>
     </div>
 
